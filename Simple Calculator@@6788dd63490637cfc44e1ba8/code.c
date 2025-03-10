b433851fec1875a9e59ca159d26410c6ef5d81eb
kashish17890 <196343500+kashish17890@users.noreply.github.com>
2025-03-10 17:45:01 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -15,7 +15,7 @@ int main()
         {printf("%d",a*b);}
         break;
         case '/':
-        if(b!=0){printf("%d",(float)a/b);}
+        if(b!=0){printf("%f",(float)a/b);}
         else{printf("error");} 
         break;
         default:
